86d985e63185409cf5a8194dd17da405f2c84cac
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-04-30 19:33:07 +0700

    Display tag in fragment_notes & fragment_note_details

--- app/src/main/java/com/hcmus/clc18se/buggynote2/adapters/BindingAdapters.java
+++ app/src/main/java/com/hcmus/clc18se/buggynote2/adapters/BindingAdapters.java
@@ -1,6 +1,7 @@
 package com.hcmus.clc18se.buggynote2.adapters;
 
 import android.graphics.Typeface;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,9 +12,12 @@ import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.hcmus.clc18se.buggynote2.R;
 import com.hcmus.clc18se.buggynote2.data.NoteWithTags;
 import com.hcmus.clc18se.buggynote2.data.Tag;
+import com.hcmus.clc18se.buggynote2.databinding.TagChipBinding;
 import com.hcmus.clc18se.buggynote2.utils.TextFormatter;
 
 import java.util.List;
@@ -121,6 +125,42 @@ public class BindingAdapters {
                                           @Nullable List<Tag> tags) {
         if (recyclerView.getAdapter() instanceof TagSelectionAdapter && tags != null) {
             ((TagSelectionAdapter) recyclerView.getAdapter()).submitList(tags);
+        }
+    }
+
+    @BindingAdapter(value = {"loadTagList", "chipLimit", "setOnClickToChips"}, requireAll = false)
+    public static void setTags(@NonNull ChipGroup chipGroup,
+                               @Nullable List<Tag> tags,
+                               @Nullable Integer limit,
+                               @Nullable View.OnClickListener clickListener) {
+
+        if (tags != null) {
+            chipGroup.removeAllViewsInLayout();
+            chipGroup.invalidate();
+            chipGroup.requestLayout();
+
+            int maximumChipAllowed = Integer.MAX_VALUE;
+            if (limit != null && limit > 0) {
+                maximumChipAllowed = limit;
+            }
+
+            for (int i = 0; i < tags.size(); ++i) {
+                if (i == maximumChipAllowed) {
+                    break;
+                }
+                Chip chip = (Chip) TagChipBinding.inflate(LayoutInflater.from(chipGroup.getContext()),
+                        chipGroup,
+                        true).getRoot();
+                if (i == maximumChipAllowed - 1 && (tags.size() - maximumChipAllowed) > 0) {
+                    String nChipRemaining = (tags.size() - maximumChipAllowed + 1) + "+";
+                    chip.setText(nChipRemaining);
+                } else {
+                    chip.setText(tags.get(i).getName());
+                }
+                if (clickListener != null) {
+                    chip.setOnClickListener(clickListener);
+                }
+            }
         }
     }
 }
