165a3f5c610db84dc69f83bd9c8b4f215b388782
Hoàng Xuân Kiệt <kiethoang611@gmail.com>
2021-05-15 20:04:57 +0700

    change StatusBar + navigationBar color

--- app/src/main/java/com/hcmus/clc18se/buggynote2/BuggyNoteActivity.java
+++ app/src/main/java/com/hcmus/clc18se/buggynote2/BuggyNoteActivity.java
@@ -11,6 +11,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.preference.PreferenceManager;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,6 +21,7 @@ import android.view.View;
 import com.google.android.material.navigation.NavigationView;
 import com.hcmus.clc18se.buggynote2.databinding.ActivityBuggyNoteBinding;
 import com.hcmus.clc18se.buggynote2.utils.OnBackPressed;
+import com.hcmus.clc18se.buggynote2.utils.ViewUtils;
 
 public class BuggyNoteActivity extends AppCompatActivity {
 
@@ -38,6 +40,17 @@ public class BuggyNoteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.getWindow().setStatusBarColor(ViewUtils.getColorAttr(
+                this, R.attr.colorPrimaryVariant
+        ));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.getWindow().setNavigationBarColor(ViewUtils.getColorAttr(
+                    this, R.attr.colorPrimaryLight
+            ));
+        }
+
 
         PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
 
