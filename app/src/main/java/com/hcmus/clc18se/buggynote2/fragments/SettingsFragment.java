154e01fb39d69d1b2cfe68bfff782e8e80424083
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-05-17 14:31:26 +0700

    Add color & theme preferences, no implemetation yet

--- app/src/main/java/com/hcmus/clc18se/buggynote2/fragments/SettingsFragment.java
+++ app/src/main/java/com/hcmus/clc18se/buggynote2/fragments/SettingsFragment.java
@@ -1,7 +1,13 @@
 package com.hcmus.clc18se.buggynote2.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.hcmus.clc18se.buggynote2.R;
