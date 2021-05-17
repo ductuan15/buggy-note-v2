5467f88b7d4e26e09b67606c1d1959be0c0a7a75
ductuan15 <47861874+ductuan15@users.noreply.github.com>
2021-05-17 21:32:41 +0700

    Add see detail option in notification and fullScreenIntent
    [+] Fix rick_roll forever
    [.] TODO: ReminderActionReceiver.java line 23

--- app/src/main/java/com/hcmus/clc18se/buggynote2/utils/ReminderActionReceiver.java
+++ app/src/main/java/com/hcmus/clc18se/buggynote2/utils/ReminderActionReceiver.java
@@ -1,6 +1,5 @@
 package com.hcmus.clc18se.buggynote2.utils;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,16 +7,22 @@ import android.widget.Toast;
 
 import androidx.core.app.NotificationManagerCompat;
 
-public class SnoozeReceiver extends BroadcastReceiver {
-    static  String ACTION_SNOOZE = "note_snooze";
+public class ReminderActionReceiver extends BroadcastReceiver {
+    public static  String ACTION_DISMISS = "note_dismiss";
+    public static  String ACTION_DETAIL = "note_detail";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().compareTo(ACTION_SNOOZE) == 0){
-            long noteID = intent.getLongExtra(ReminderReceiver.NOTE_ID_TIME_KEY,0);
+        if(intent.getAction().compareTo(ACTION_DISMISS) == 0){
+            long noteID = intent.getLongExtra(ReminderReceiver.NOTE_ID_KEY,0);
             ReminderMusicControl.getInstance(context).stopMusic();
             NotificationManagerCompat  notificationManager =  NotificationManagerCompat.from(context);
             notificationManager.cancel((int)noteID);
             Toast.makeText(context,"Dismiss",Toast.LENGTH_LONG).show();
+        }else if(intent.getAction().compareTo(ACTION_DETAIL) == 0){
+            //TODO: cần thận rick_roll không ngừng
+            long noteID = intent.getLongExtra(ReminderReceiver.NOTE_ID_KEY,0);
+            Toast.makeText(context,"Detail",Toast.LENGTH_LONG).show();
         }
     }
 }
