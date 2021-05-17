bb2f8cc25c244783dffe91eddaec2137d46567ae
ductuan15 <47861874+ductuan15@users.noreply.github.com>
2021-05-17 14:54:48 +0700

    fix reminder play twice + make screen on if it is current off

--- app/src/main/java/com/hcmus/clc18se/buggynote2/AlarmActivity.java
+++ app/src/main/java/com/hcmus/clc18se/buggynote2/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.hcmus.clc18se.buggynote2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 import com.google.android.material.chip.Chip;
 import com.hcmus.clc18se.buggynote2.utils.ReminderMusicControl;
 import com.hcmus.clc18se.buggynote2.utils.ReminderReceiver;
+import com.hcmus.clc18se.buggynote2.utils.SnoozeReceiver;
 
 import org.w3c.dom.Text;
 
@@ -24,16 +27,20 @@ import org.w3c.dom.Text;
 public class AlarmActivity extends AppCompatActivity implements View.OnClickListener {
     Chip button;
     String noteTitle = "";
-    String noteDateTime= "";
+    String noteDateTime = "";
+    Long noteID ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_alarm);
-        ReminderMusicControl.getInstance(this).playMusic(this);
         getData();
         bindingView();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
 
 
     }
@@ -41,7 +48,11 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (v == button) {
-            ReminderMusicControl.getInstance(this).stopMusic();
+            Intent intent = new Intent(getBaseContext(), SnoozeReceiver.class);
+            Bundle bundle = new Bundle();
+            bundle.putLong(ReminderReceiver.NOTE_ID_TIME_KEY,noteID);
+            sendBroadcast(intent);
+            Toast.makeText(getBaseContext(), "Dismissed", Toast.LENGTH_SHORT).show();
             finish();
         }
     }
@@ -57,12 +68,13 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
         dateTimeReminderTextView.setText(noteDateTime);
     }
 
-    public void getData(){
+    public void getData() {
         Intent intent = getIntent();
-        if(intent!= null){
+        if (intent != null) {
             Bundle bundle = intent.getExtras();
             noteTitle = bundle.getString(ReminderReceiver.NOTE_TITLE_KEY);
             noteDateTime = bundle.getString(ReminderReceiver.NOTE_DATE_TIME_KEY);
+            noteID = bundle.getLong(ReminderReceiver.NOTE_ID_TIME_KEY);
         }
     }
 }
